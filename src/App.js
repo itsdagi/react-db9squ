5e014739017ecee7a3e0675e728be66429db1716
dagimalemux <dagimalemux@gmail.com>
2023-12-30 21:36:10 +0300

    commit it

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./style.css";
 export default function App() {
   return (
     <div>
-      <h1>Hello Dagim!</h1>
+      <h1>Hello Dagim*!</h1>
       <p> Lets start the journey of React Js :)</p>
     </div>
   );
