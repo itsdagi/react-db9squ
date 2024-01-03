e53cd0abc7b5102c10de95bb2c17958781887072
dagimalemux <dagimalemux@gmail.com>
2024-01-03 17:14:54 +0300

    recap

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import App from './App'
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(
     <React.StrictMode>
-        <h1>
-            Hello!
-        </h1>
+        <App />
     </React.StrictMode>
 )
