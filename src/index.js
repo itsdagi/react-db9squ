2da4f275414193a98511f7ef5e376b2fba88c691
dagimalemux <dagimalemux@gmail.com>
2024-01-03 15:56:00 +0300

    commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(
+    <React.StrictMode>
+        <h1>Dagim</h1>
+        <p>hello dag</p>
+     </React.StrictMode>
+    
+)
