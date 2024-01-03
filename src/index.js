dd2b528894791ddd77de2e2f3bcc3c3279eaa418
dagimalemux <dagimalemux@gmail.com>
2024-01-03 17:00:03 +0300

    recap

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-
-
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(
     <React.StrictMode>
-        <h1>Dagim</h1>
-        <p>hello dag</p>
-     </React.StrictMode>
-    
+        <h1>
+            Hello!
+        </h1>
+    </React.StrictMode>
 )
